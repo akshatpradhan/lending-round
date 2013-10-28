ddb74e4f7a7e82e51394c46a96acf87c6df26d4b
Akshat Pradhan <akshat.ip@gmail.com>
2013-10-28 14:13:56 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LendingRound::Application.config.secret_token = '3717f06f232aba61564e7197fa39d854639dea910e9cdf19a8dbbcae8f0e7af819ba9bc5c60eef89ab10bf6587697dff5215fa4c7375de04c67c9caf5701a775'
