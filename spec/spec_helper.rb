aca2a27398ffa9ac5c598d2b9f64b458e219fd17
Andrea D'Amico <andrea.damico@gmail.com>
2013-10-29 11:34:32 +0100

    use factorygirl methods without class prefix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
+  config.include FactoryGirl::Syntax::Methods
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
