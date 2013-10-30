9e4dd37686227da084c9cd3706875756c0223cfd
Akshat Pradhan <akshat.ip@gmail.com>
2013-10-30 06:45:29 -0400

    change omniauth environment variable namespace

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :dwolla, ENV['DWOLLA_KEY'], ENV['DWOLLA_SECRET'], :scope => 'accountinfofull|send|request', :provider_ignores_state => true
+  provider :dwolla, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], :scope => 'accountinfofull|send|request', :provider_ignores_state => true
 end
