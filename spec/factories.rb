bdd51cb953e1c270dd49dfb9b0fa493247f18f22
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-07 12:16:04 +0100

    adding note factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,17 @@ FactoryGirl.define do
     sequence(:name) { |n| "user#{n}"}
     email { |user| "#{user.name}@example.com".downcase}
   end
+
+  factory :note do
+    amount 6000
+    rate   11.0
+    term 36
+    start_date Time.now.to_date
+    lender_name "Bob Raymond"
+    lender_email "bobraymond@gmail.com"
+    lender_address "270 Boylston St Boston MA 02116"
+    borrower_name "Terry Nichols"
+    borrower_email "terrynichols@gmail.com"
+    borrower_address "1358 Richard Dr Boston MA 02115"
+  end
 end
