591a196dc7fee49452f73549d519fcb190c2f7bf
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-14 10:50:49 +0100

    passing spec for js loan summary generation

--- spec/features/loan_summary_spec.rb
+++ spec/features/loan_summary_spec.rb
@@ -7,10 +7,10 @@ feature "User wanting to create a loan" do
     fill_in "note_amount", with: "3000"
     fill_in "note_rate", with: "13.17"
     fill_in "note_term", with: "12"
-    fill_in "note_start_date", with: "12/01/2013"
+    fill_in "note_start_date", with: "2013-12-01"
     page.should have_content "$268.19"
-    page.should have_content "$3,218.29"
-    page.should have_content "$218.29"
-    page.should have_content "Nov. 2014"
+    page.should have_content "$3,218.28"
+    page.should have_content "$218.28"
+    page.should have_content "Dec. 2014"
   end
 end
